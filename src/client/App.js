843e59f6d9b8a8f66bdae476b3813a7c678fb19a
sveinhov <47802108+sveinhov@users.noreply.github.com>
2020-08-03 21:14:47 -0500

    testing cards. component not working

--- src/client/App.js
+++ src/client/App.js
@@ -1,7 +1,11 @@
 /* eslint-disable linebreak-style */
 import React, { Component } from 'react';
 import './app.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import { SensorCard } from './cards'
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck'
+import { SensorCard } from './cards';
 
 export default class App extends Component {
   state = { username: null };
@@ -14,10 +18,47 @@ export default class App extends Component {
 
   render() {
     const { username } = this.state;
+    const SBP1 = 1.23;
     return (
       <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
+        <h1>HMI testing</h1>
+        <CardDeck>
+        <Card 
+        bg='secondary'
+        text='light'
+        style={{ width: '7rem' }}>
+        <Card.Body>
+          <Card.Title as = "h2">{SBP1} psi </Card.Title>          
+          <Card.Text>
+            SPP
+          </Card.Text>          
+        </Card.Body>
+      </Card>
+      <Card 
+        bg='secondary'
+        text='light'
+        style={{ width: '7rem' }}>
+        <Card.Body>
+          <Card.Title as = "h2">{SBP1} psi </Card.Title>          
+          <Card.Text>
+            SBP
+          </Card.Text>          
+        </Card.Body>
+      </Card>
+      <Card 
+        bg='secondary'
+        text='light'
+        style={{ width: '7rem' }}>
+        <Card.Body>
+          <Card.Title as = "h2">100 % </Card.Title>          
+          <Card.Text>
+            POS
+          </Card.Text>          
+        </Card.Body>
+      </Card>
+      </CardDeck>
       </div>
     );
   }
 }
+// {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
---
+++ src/client/cards.js
@@ -0,0 +1,23 @@
+import Card from 'react-bootstrap/Card';
+
+  function SensorCard(props) {
+    return <h1>Hello, svein</h1>;
+  }
+
+
+export { SensorCard }
+/* <div>
+        <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+            the card's content.
+          </Card.Text>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
+      </div>
+      */
